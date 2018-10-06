36ff1c90b4c263023503f4cbd3b0343316487012
iqzap <iqzap@it.student.pens.ac.id>
2018-10-06 23:10:25 +0700

    wificorner-sabtu

--- app/src/main/java/com/marketplace/kelompok2/kue/ui/home/fragmentwishlist/WishlistFragment.java
+++ app/src/main/java/com/marketplace/kelompok2/kue/ui/home/fragmentwishlist/WishlistFragment.java
@@ -1,7 +1,25 @@
 package com.marketplace.kelompok2.kue.ui.home.fragmentwishlist;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.marketplace.kelompok2.kue.R;
 
 public class WishlistFragment extends Fragment {
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_wishlist, container, false);
+        return rootView;
+    }
 }
